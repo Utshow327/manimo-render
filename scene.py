739e93a4b43acd7284235f4bb1309c68825123c9
Utshow327 <utshow.drmc@gmail.com>
2026-02-27 12:59:49 +0600

    render 724ed3305d16

--- scene.py
+++ scene.py
@@ -4,4 +4,4 @@ class MainScene(Scene):
     def construct(self):
         circle = Circle(color=BLUE, fill_opacity=0.5)
         self.play(Create(circle))
-        self.wait(1)
+        self.wait(3)
