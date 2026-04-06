aa64f23093e62a109b4a10c3721d443acc881e36
Utshow327 <utshow.drmc@gmail.com>
2026-04-06 23:02:12 +0600

    Test render: circle

--- scene.py
+++ scene.py
@@ -1,25 +1,7 @@
 from manim import *
 
-class SimpleAnimation(Scene):
+class MainScene(Scene):
     def construct(self):
-        # Create text
-        title = Text("Simple Manim Animation")
-        self.play(Write(title))
-        self.wait(1)
-
-        # Move text to top
-        self.play(title.animate.to_edge(UP))
-
-        # Create a circle
-        circle = Circle()
+        circle = Circle(color=BLUE, fill_opacity=0.5)
         self.play(Create(circle))
-        self.wait(1)
-
-        # Transform circle into square
-        square = Square()
-        self.play(Transform(circle, square))
-        self.wait(1)
-
-        # Fade everything out
-        self.play(FadeOut(title), FadeOut(circle))
         self.wait(1)
