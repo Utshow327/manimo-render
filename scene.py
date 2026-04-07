42574a2f4dbd40dd9f811c0e78ac92176b75bce1
Utshow327 <utshow.drmc@gmail.com>
2026-04-07 14:41:05 +0600

    render 01b1e08c19e1

--- scene.py
+++ scene.py
@@ -2,64 +2,48 @@ from manim import *
 
 class MainScene(Scene):
     def construct(self):
-        # Introduction
-        intro_text = Text("Conics: An Introduction")
-        self.play(Write(intro_text))
-        self.wait(2)
-        self.play(FadeOut(intro_text))
+        # Set the aspect ratio to 16:9
+        self.camera.aspect_ratio = 16/9
 
-        # Conic Sections
-        conic_sections_text = Text("Conic Sections")
-        self.play(Write(conic_sections_text))
-        self.wait(2)
-        self.play(FadeOut(conic_sections_text))
+        # Create a title for the scene
+        title = Title("Conics: Circles, Ellipses, Parabolas, and Hyperbolas")
+        self.play(Write(title))
 
-        # Circle
-        circle_text = Text("Circle: The set of all points that are at a fixed distance (radius) from a center point.")
-        self.play(Write(circle_text))
-        self.wait(2)
-        self.play(FadeOut(circle_text))
-
+        # Create a circle
         circle = Circle(radius=2, color=BLUE)
-        self.play(Create(circle))
-        self.wait(2)
+        self.play(FadeIn(circle))
+        self.wait(1)
         self.play(FadeOut(circle))
 
-        # Ellipse
-        ellipse_text = Text("Ellipse: The set of all points for which the sum of the distances to two fixed points (foci) is constant.")
-        self.play(Write(ellipse_text))
-        self.wait(2)
-        self.play(FadeOut(ellipse_text))
-
-        ellipse = Ellipse(width=4, height=2, color=RED)
-        self.play(Create(ellipse))
-        self.wait(2)
+        # Create an ellipse
+        ellipse = Ellipse(width=4, height=2, color=GREEN)
+        self.play(FadeIn(ellipse))
+        self.wait(1)
         self.play(FadeOut(ellipse))
 
-        # Parabola
-        parabola_text = Text("Parabola: The set of all points that are equidistant to a fixed point (focus) and a fixed line (directrix).")
-        self.play(Write(parabola_text))
-        self.wait(2)
-        self.play(FadeOut(parabola_text))
-
-        parabola = FunctionGraph(lambda x: x**2, x_min=-2, x_max=2, color=YELLOW)
-        self.play(Create(parabola))
-        self.wait(2)
+        # Create a parabola
+        parabola = ParametricFunction(lambda t: (t**2, t), t_range=[-2, 2], color=RED)
+        self.play(FadeIn(parabola))
+        self.wait(1)
         self.play(FadeOut(parabola))
 
-        # Hyperbola
-        hyperbola_text = Text("Hyperbola: The set of all points for which the difference of the distances to two fixed points (foci) is constant.")
-        self.play(Write(hyperbola_text))
-        self.wait(2)
-        self.play(FadeOut(hyperbola_text))
-
-        hyperbola = FunctionGraph(lambda x: 1/x, x_min=0.1, x_max=2, color=GREEN)
-        self.play(Create(hyperbola))
-        self.wait(2)
+        # Create a hyperbola
+        hyperbola = ParametricFunction(lambda t: (t, 1/t), t_range=[0.1, 10], color=YELLOW)
+        self.play(FadeIn(hyperbola))
+        self.wait(1)
         self.play(FadeOut(hyperbola))
 
-        # Conclusion
-        conclusion_text = Text("Conclusion: Conic sections are fundamental concepts in mathematics and physics, and understanding them is essential for various applications.")
-        self.play(Write(conclusion_text))
+        # Create a graph with multiple conics
+        graph = VGroup(
+            Circle(radius=1, color=BLUE).shift(LEFT * 3),
+            Ellipse(width=2, height=1, color=GREEN).shift(UP * 2),
+            ParametricFunction(lambda t: (t**2, t), t_range=[-2, 2], color=RED).shift(RIGHT * 2),
+            ParametricFunction(lambda t: (t, 1/t), t_range=[0.1, 10], color=YELLOW).shift(DOWN * 2),
+        )
+        self.play(FadeIn(graph))
         self.wait(2)
-        self.play(FadeOut(conclusion_text))
+        self.play(FadeOut(graph))
+
+        # End the scene
+        self.play(FadeOut(title))
+        self.wait(1)
