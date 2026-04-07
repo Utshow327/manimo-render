90d60c1fb8004d1d5420eb61e8a5d118d2d502ad
Utshow327 <utshow.drmc@gmail.com>
2026-04-07 14:38:07 +0600

    render d2b63685a142

--- scene.py
+++ scene.py
@@ -2,25 +2,64 @@ from manim import *
 
 class MainScene(Scene):
     def construct(self):
-        # 1. Setup objefasdfcts
-        circle = Cirdsacle(radius=2, color=BLUE)
-        text = Textfsad("12 Second Timer", font_size=36).to_edge(UP)
-xd
-        # 2. Start Animations
-        # Animation 1: Write text (2 seconds)
-        self.play(Wasdfrite(teasdfxt), run_time=2)
-        fas sd
-        # Animation 2: Create circle (2 seconds)
-        self.play(Create(as df sadf safsdaf scircle), run_time=2)
-        
-        # Animation 3: Transform circle to square (2 seconds)
-        square = Square(side_length=4, color=RED) # corrected the side length to match the circle's diameter
-        self.play(ReplacementTsd fwsf asdransform(circle, square), run_time=2)
-        
-        # Animation fsadf4: Rotate the square (3 seconds)
-        self.play(Rotate(square, angle=PI*2), run_time=3)
-        sd
-        # 5. Final Wait (3 seconds)
-        # Total so sadffar:f 2 + 2 + 2 + 3 = 9 seconds. 
-        # We need 3 more seconds to reach 12.
-        self.wait(3)asdf saf
+        # Introduction
+        intro_text = Text("Conics: An Introduction")
+        self.play(Write(intro_text))
+        self.wait(2)
+        self.play(FadeOut(intro_text))
+
+        # Conic Sections
+        conic_sections_text = Text("Conic Sections")
+        self.play(Write(conic_sections_text))
+        self.wait(2)
+        self.play(FadeOut(conic_sections_text))
+
+        # Circle
+        circle_text = Text("Circle: The set of all points that are at a fixed distance (radius) from a center point.")
+        self.play(Write(circle_text))
+        self.wait(2)
+        self.play(FadeOut(circle_text))
+
+        circle = Circle(radius=2, color=BLUE)
+        self.play(Create(circle))
+        self.wait(2)
+        self.play(FadeOut(circle))
+
+        # Ellipse
+        ellipse_text = Text("Ellipse: The set of all points for which the sum of the distances to two fixed points (foci) is constant.")
+        self.play(Write(ellipse_text))
+        self.wait(2)
+        self.play(FadeOut(ellipse_text))
+
+        ellipse = Ellipse(width=4, height=2, color=RED)
+        self.play(Create(ellipse))
+        self.wait(2)
+        self.play(FadeOut(ellipse))
+
+        # Parabola
+        parabola_text = Text("Parabola: The set of all points that are equidistant to a fixed point (focus) and a fixed line (directrix).")
+        self.play(Write(parabola_text))
+        self.wait(2)
+        self.play(FadeOut(parabola_text))
+
+        parabola = FunctionGraph(lambda x: x**2, x_min=-2, x_max=2, color=YELLOW)
+        self.play(Create(parabola))
+        self.wait(2)
+        self.play(FadeOut(parabola))
+
+        # Hyperbola
+        hyperbola_text = Text("Hyperbola: The set of all points for which the difference of the distances to two fixed points (foci) is constant.")
+        self.play(Write(hyperbola_text))
+        self.wait(2)
+        self.play(FadeOut(hyperbola_text))
+
+        hyperbola = FunctionGraph(lambda x: 1/x, x_min=0.1, x_max=2, color=GREEN)
+        self.play(Create(hyperbola))
+        self.wait(2)
+        self.play(FadeOut(hyperbola))
+
+        # Conclusion
+        conclusion_text = Text("Conclusion: Conic sections are fundamental concepts in mathematics and physics, and understanding them is essential for various applications.")
+        self.play(Write(conclusion_text))
+        self.wait(2)
+        self.play(FadeOut(conclusion_text))
