0b1fedd51aa686d29f463bb798f4ad984c3cc427
Utshow327 <utshow.drmc@gmail.com>
2026-05-11 15:28:39 +0600

    render ba67a9c87e00

--- scene.py
+++ scene.py
@@ -2,100 +2,96 @@ from manim import *
 
 class MainScene(Scene):
     def construct(self):
-        # Introduction
-        intro_text = Text("Fourier Transform", font_size=64)
-        self.play(FadeIn(intro_text))
-        self.wait(2)
-        self.play(FadeOut(intro_text))
+        # Create the sun
+        sun = Circle(color=YELLOW, radius=1.5)
+        sun.set_fill(YELLOW, opacity=1)
+        self.add(sun)
 
-        # Time domain signal
-        time_domain_signal = Axes(
-            x_range=[0, 10, 1],
-            y_range=[-1, 1, 0.5],
-            x_length=8,
-            y_length=4,
-            axis_config={"include_tip": False},
-        )
-        time_domain_signal_graph = time_domain_signal.plot(
-            lambda x: np.sin(x) + 0.5 * np.sin(3 * x), x_range=[0, 10, 0.01], color=BLUE
-        )
-        time_domain_signal_text = Text("Time Domain Signal", font_size=32)
-        time_domain_signal_text.next_to(time_domain_signal, UP)
-        self.play(FadeIn(time_domain_signal), Create(time_domain_signal_graph), FadeIn(time_domain_signal_text))
-        self.wait(2)
+        # Create the planets
+        mercury = Circle(color=GREY, radius=0.2)
+        mercury.set_fill(GREY, opacity=1)
+        mercury.shift(2 * LEFT)
 
-        # Frequency domain signal
-        frequency_domain_signal = Axes(
-            x_range=[0, 10, 1],
-            y_range=[0, 2, 0.5],
-            x_length=8,
-            y_length=4,
-            axis_config={"include_tip": False},
-        )
-        frequency_domain_signal_graph = frequency_domain_signal.plot(
-            lambda x: np.abs(np.fft.fft(np.sin(np.linspace(0, 10, 1000)) + 0.5 * np.sin(3 * np.linspace(0, 10, 1000)))),
-            x_range=[0, 10, 0.01],
-            color=RED,
-        )
-        frequency_domain_signal_text = Text("Frequency Domain Signal", font_size=32)
-        frequency_domain_signal_text.next_to(frequency_domain_signal, UP)
-        self.play(
-            Transform(time_domain_signal, frequency_domain_signal),
-            Transform(time_domain_signal_graph, frequency_domain_signal_graph),
-            Transform(time_domain_signal_text, frequency_domain_signal_text),
-        )
-        self.wait(2)
+        venus = Circle(color=WHITE, radius=0.4)
+        venus.set_fill(WHITE, opacity=1)
+        venus.shift(3.5 * LEFT)
 
-        # Transform explanation
-        transform_explanation_text = Text("Fourier Transform: Time → Frequency", font_size=48)
-        self.play(FadeIn(transform_explanation_text))
-        self.wait(2)
-        self.play(FadeOut(transform_explanation_text))
+        earth = Circle(color=BLUE, radius=0.5)
+        earth.set_fill(BLUE, opacity=1)
+        earth.shift(5 * LEFT)
 
-        # Inverse Fourier Transform
-        inverse_fourier_transform_text = Text("Inverse Fourier Transform: Frequency → Time", font_size=48)
-        self.play(FadeIn(inverse_fourier_transform_text))
-        self.wait(2)
-        self.play(FadeOut(inverse_fourier_transform_text))
+        mars = Circle(color=RED, radius=0.3)
+        mars.set_fill(RED, opacity=1)
+        mars.shift(6.5 * LEFT)
 
-        # Example of Fourier Transform application
-        example_text = Text("Example: Filtering a Signal", font_size=48)
-        self.play(FadeIn(example_text))
-        self.wait(2)
+        jupiter = Circle(color="#964B00", radius=1)
+        jupiter.set_fill("#964B00", opacity=1)
+        jupiter.shift(9 * LEFT)
 
-        # Filtering a signal
-        signal = Axes(
-            x_range=[0, 10, 1],
-            y_range=[-1, 1, 0.5],
-            x_length=8,
-            y_length=4,
-            axis_config={"include_tip": False},
-        )
-        signal_graph = signal.plot(
-            lambda x: np.sin(x) + 0.5 * np.sin(3 * x) + 0.1 * np.sin(10 * x), x_range=[0, 10, 0.01], color=BLUE
-        )
-        filtered_signal = Axes(
-            x_range=[0, 10, 1],
-            y_range=[-1, 1, 0.5],
-            x_length=8,
-            y_length=4,
-            axis_config={"include_tip": False},
-        )
-        filtered_signal_graph = filtered_signal.plot(
-            lambda x: np.sin(x) + 0.5 * np.sin(3 * x), x_range=[0, 10, 0.01], color=RED
-        )
-        self.play(FadeIn(signal), Create(signal_graph))
-        self.wait(2)
-        self.play(
-            Transform(signal, filtered_signal),
-            Transform(signal_graph, filtered_signal_graph),
-        )
-        self.wait(2)
+        saturn = Circle(color="#C9C4B5", radius=0.8)
+        saturn.set_fill("#C9C4B5", opacity=1)
+        saturn.shift(11.5 * LEFT)
 
-        # Conclusion
-        conclusion_text = Text("Conclusion: Fourier Transform is a powerful tool for signal processing", font_size=48)
-        self.play(FadeIn(conclusion_text))
-        self.wait(2)
-        self.play(FadeOut(conclusion_text))
+        uranus = Circle(color="#56B3FA", radius=0.6)
+        uranus.set_fill("#56B3FA", opacity=1)
+        uranus.shift(14 * LEFT)
+
+        neptune = Circle(color="#2E4053", radius=0.5)
+        neptune.set_fill("#2E4053", opacity=1)
+        neptune.shift(16.5 * LEFT)
+
+        # Create the orbits
+        mercury_orbit = Circle(color=GREY, radius=2)
+        mercury_orbit.set_stroke(width=0.5)
+
+        venus_orbit = Circle(color=WHITE, radius=3.5)
+        venus_orbit.set_stroke(width=0.5)
+
+        earth_orbit = Circle(color=BLUE, radius=5)
+        earth_orbit.set_stroke(width=0.5)
+
+        mars_orbit = Circle(color=RED, radius=6.5)
+        mars_orbit.set_stroke(width=0.5)
+
+        jupiter_orbit = Circle(color="#964B00", radius=9)
+        jupiter_orbit.set_stroke(width=0.5)
+
+        saturn_orbit = Circle(color="#C9C4B5", radius=11.5)
+        saturn_orbit.set_stroke(width=0.5)
+
+        uranus_orbit = Circle(color="#56B3FA", radius=14)
+        uranus_orbit.set_stroke(width=0.5)
+
+        neptune_orbit = Circle(color="#2E4053", radius=16.5)
+        neptune_orbit.set_stroke(width=0.5)
+
+        # Add the planets and orbits to the scene
+        self.add(mercury_orbit)
+        self.add(venus_orbit)
+        self.add(earth_orbit)
+        self.add(mars_orbit)
+        self.add(jupiter_orbit)
+        self.add(saturn_orbit)
+        self.add(uranus_orbit)
+        self.add(neptune_orbit)
+
+        self.add(mercury)
+        self.add(venus)
+        self.add(earth)
+        self.add(mars)
+        self.add(jupiter)
+        self.add(saturn)
+        self.add(uranus)
+        self.add(neptune)
+
+        # Animate the planets moving along their orbits
+        self.play(MoveAlongPath(mercury, mercury_orbit), rate_func=linear, run_time=10)
+        self.play(MoveAlongPath(venus, venus_orbit), rate_func=linear, run_time=15)
+        self.play(MoveAlongPath(earth, earth_orbit), rate_func=linear, run_time=20)
+        self.play(MoveAlongPath(mars, mars_orbit), rate_func=linear, run_time=25)
+        self.play(MoveAlongPath(jupiter, jupiter_orbit), rate_func=linear, run_time=30)
+        self.play(MoveAlongPath(saturn, saturn_orbit), rate_func=linear, run_time=35)
+        self.play(MoveAlongPath(uranus, uranus_orbit), rate_func=linear, run_time=40)
+        self.play(MoveAlongPath(neptune, neptune_orbit), rate_func=linear, run_time=45)
 
         self.wait(15)
